3f9f5ba820452db49d17a59883b128e1d5fb783b
windows channel <info@windowschannel.com>
2022-09-15 22:49:07 -0500

    add services for topics globals, restart and newstreaming

--- services/players/actions.js
+++ services/players/actions.js
@@ -1,14 +1,35 @@
 const axios = require('axios')
 const { hostBroker , route , auth } = require('../../config')
 
-axios.get(`${hostBroker}/${route}`, { auth })
-    .then(function(response){
-        if(response.status === 200){
-            const players = response
-            console.log(players.data);
-        }else{
-            console.log(response.status);
-        }
-    }).catch(e =>{
-        console.log(e);
-    })
+function newStreamingPlayers(data){ 
+    const {client, newstreaming} = data
+    const topic = `p${client}/`
+    console.log(client);
+    axios.get(`${hostBroker}/${route}`, { auth })
+        .then((response) =>{
+            if (response.status === 200) {
+                let playersfound = []
+                const players = response.data.data
+                // console.log(players);
+                players.find(player => {
+                    if (client.name === player.username) {
+                        playersfound.push({
+                            "clientid": player.clientid,
+                            "username": player.username,
+                            "connected_at": player.connected_at,
+                            "subscriptions_cnt": player.subscriptions_cnt,
+                            "connected": player.connected,
+                            "ip_address": player.ip_address
+                        })
+                    }
+                })
+                return playersfound
+            }
+        }).then((players)=>{
+            
+        })
+}
+
+module.exports = {
+    newStreamingPlayers,
+}
--- services/players/globalActions.js
+++ services/players/globalActions.js
@@ -1,26 +1,53 @@
 const axios = require('axios')
 const { hostBroker , route , auth } = require('../../config')
 
+function restartPlayers(data){ 
+    const { client } = data
+    const topic = `${ client.name }/players`
+
+    axios.post(`${hostBroker}/api/v4/mqtt/publish`, {
+        "topic": topic,
+        "payload":{ 
+            "restart": "device"
+         },
+        "qos":0,
+        "retain":false,
+        },{ auth }).then((response) =>{
+        if (response.status === 200) {
+            console.log(' Mensaje enviado ');
+        }else{
+            console.log('l');
+        }
+    }).catch(e => console.log({
+        code: e.code,
+        statusCode: e.response.status,
+        statusMessage: e.response.statusText
+    }))
+}
 
 function newStreamingPlayers(data){ 
-    const {client, newstreaming} = data
-    console.log(client);
-    axios.get(`${hostBroker}/${route}`, { auth })
-        .then((response) =>{
-            if (response.status === 200) {
-                let playersfound = []
-                const players = response.data.data
-                // console.log(players);
-                players.find(player => {
-                    if (client.name === player.username) {
-                        playersfound.push(player)
-                    }
-                })
-                
-            }
-        })
+    const { client, newstreaming } = data
+    const topic = `${client.name}/players`
+
+    axios.post(`${hostBroker}/api/v4/mqtt/publish`, {
+        "topic": topic,
+        "payload": newstreaming,
+        "qos":0,
+        "retain":false,
+        },{ auth }).then((response) =>{
+        if (response.status === 200) {
+            console.log('Mensaje enviado');
+        }else{
+            console.log('l');
+        }
+    }).catch(e => console.log({
+        code: e.code,
+        statusCode: e.response.status,
+        statusMessage: e.response.statusText
+    }))
 }
 
 module.exports = {
     newStreamingPlayers,
+    restartPlayers
 }
