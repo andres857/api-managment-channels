cf0edc26fc935cc84816dd6a4e774690e0679b8a
windows channel <info@windowschannel.com>
2022-10-09 23:18:16 -0500

    minnor changes

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
+var cors = require('cors');
 const { getPlayersByClient } = require('./services/players/getPlayers')
 const { newStreamingPlayers } = require('./services/players/globalActions')
 const port = 3000
 
+app.use( cors() )
 app.use( express.json() )
 app.use( express.urlencoded({ extended: false }) );
 
+app.get('/v1/streaming/wc/info',(req,res)=>{
+    const {client} = req.query
+    getPlayersByClient(client)
+        .then((players)=>{
+            console.log(players);
+            res.json(players)
+        }).catch(e=> console.log(e))
+})
 // get Players
 app.get('/v1/streaming/wc/players', (req, res) => {
     const { client } = req.body
